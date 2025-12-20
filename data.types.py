ad63238c7b180d5ac83ad39114ca153a9f5baf4e
satyadeepthupukar25-TSDP <satyadeepthupukar25@gmail.com>
2025-12-20 16:56:20 +0530

    Add files via upload

---
+++ data.types.py
@@ -0,0 +1,14 @@
+#data tyues - in python 3 data types
+#1.int(integer)
+#2.str(string)
+#3.float(decimals)
+name="venkat123"
+age=30
+salary=10000"123
+data="1234"
+print (name)
+print(type(name))
+print(age)
+print(type(age ))
+print(salary)
+print(tpe(salary))
